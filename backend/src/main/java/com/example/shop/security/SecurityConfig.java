cae98807e413f64bec364ba0979ba0bd229fac20
masion maison <arthurnemangou@gmail.com>
2026-02-24 16:00:26 +0100

    Allow-favicon

--- backend/src/main/java/com/example/shop/security/SecurityConfig.java
+++ backend/src/main/java/com/example/shop/security/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/uploads/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/").permitAll()
+                        .requestMatchers("/favicon.ico").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider)
